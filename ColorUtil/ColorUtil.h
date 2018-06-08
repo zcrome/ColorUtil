eb8bd8868b47cb89db53b5315fa0d67e1e3fa7fa
zcrome <soportezcrome@gmail.com>
2018-06-08 11:46:24 -0500

    Initial Commit

---
+++ ColorUtil/ColorUtil.h
@@ -0,0 +1,19 @@
+//
+//  ColorUtil.h
+//  ColorUtil
+//
+//  Created by Melanie on 6/8/18.
+//  Copyright © 2018 Eduardo Palomino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ColorUtil.
+FOUNDATION_EXPORT double ColorUtilVersionNumber;
+
+//! Project version string for ColorUtil.
+FOUNDATION_EXPORT const unsigned char ColorUtilVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ColorUtil/PublicHeader.h>
+
+
